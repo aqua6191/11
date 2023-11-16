032d25287c38dbbba57ff794cc4b0bddc5453596
양지원 <101974118+aqua6191@users.noreply.github.com>
2023-11-16 15:42:01 +0900

    03

--- main.c
+++ main.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void) {
-    int i = 300; //300ver, 4verµµ ÇØº½  
-    
-    int *pi = &i;
-    char *pc = &i;
-    
-    printf("%i,%i,&i\n",i,*pi,*pc);
-    
+void swap (int *x, int *y) {
+     int temp;
+     temp = *x;
+     *x = *y;
+     *y = temp;
+     }
+     
+void main(void) {
+     int a = 3;
+     int b = 5;
+     
+     swap(&a, &b);
+     printf("a:%i, b:%i\n", a, b);
+
 
   system("PAUSE");	
   return 0;
